9074bab89d428018d26913573310b922c3d82198
Derine Banis <d.banis.13@gmail.com>
2022-10-27 19:05:14 -0700

    Use trained data downloanded from tessdata_best

--- tesseract.js/index.js
+++ tesseract.js/index.js
@@ -6,7 +6,7 @@ const textScanner = async() => {
   await worker.load();
   await worker.loadLanguage('eng');
   await worker.initialize('eng');
-  const {data: {text}} = await worker.recognize('pictures/steamtest4.jpg')
+  const {data: {text}} = await worker.recognize('pictures/djmaxtest3.jpg')
   console.log(text);
   await worker.terminate();
 }
