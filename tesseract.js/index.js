3ff99fba7b8bf2af5af7b1d0ead267d2618fa2a8
Derine Banis <d.banis.13@gmail.com>
2022-10-28 16:30:56 -0700

    Experiment with tesseract

--- tesseract.js/index.js
+++ tesseract.js/index.js
@@ -1,12 +1,13 @@
-const {createWorker} = require('tesseract.js')
+const {createWorker, } = require('tesseract.js')
 
 const worker = createWorker();
 
 const textScanner = async() => {
   await worker.load();
-  await worker.loadLanguage('eng');
-  await worker.initialize('eng');
-  const {data: {text}} = await worker.recognize('pictures/djmaxtest3.jpg')
+  await worker.loadLanguage('eng+kor');
+  await worker.initialize('eng+kor');
+
+  const {data: {text}} = await worker.recognize('pictures/djmaxtest3.jpg', {rectangle: {top: 0, left: 0, height: 500, width: 300}})
   console.log(text);
   await worker.terminate();
 }
