f3ee0c31dd47942d5ecbdeb064be3b5f73f3783c
Derine Banis <d.banis.13@gmail.com>
2022-11-29 12:59:22 -0800

    Practicing reading csv data

--- csvReader.js
+++ csvReader.js
@@ -1,12 +1,21 @@
 const fs = require('fs')
 
-fs.readFile('testCSV.csv', 'utf-8', (err,data)=>{
+fs.readFile('test2.csv', 'utf-8', (err,data)=>{
   let something = data.split('\n')
-  console.log(something)
   const songs = [];
-  something.shift()
+  // something.shift()
   
-  // something.forEach(song => {
-
-  // })
+  something.forEach(data => {
+    
+    const song = data.split(',')
+    const title = song[0]
+    const genre = song[1]
+    const composer = song[2]
+    const visualizer = song[3]
+    const vocal = song[4]
+    const dlc = String(song[5]).toLowerCase().includes('dlc')
+    // const dlc = song[5]
+    songs.push({title,genre,composer,visualizer,vocal,dlc})
+  })
+  console.log(songs)
 })
